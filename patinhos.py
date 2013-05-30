08ee4f597373267b8e47a391d9c0500dab991818
leo523 <leonardo_tada@hotmail.com>
2013-05-30 18:41:44 -0300

    Update patinhos.py
    O código não está muito bonito, mas um exemplo fácil de se fazer.

--- patinhos.py
+++ patinhos.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#
+# Minha versão em Python da música da Xuxa, Cinco Patinhos, retirado deste blog de um amigo feito em C#
+# http://techblog.desenvolvedores.net/2012/03/03/xuxa-para-programadores/
+
 
 def quack():
     "Repete qua! 4 vezes"
@@ -6,15 +10,13 @@ def quack():
 
 patos = 5
 while patos > 0:
-    print str(patos) + (' patinhos foram passear' if patos != 1
-        else ' patinho foi passear')
+    print str(patos) + (' patinhos foram passear' if patos != 1 else ' patinho foi passear')
     print 'além das montanhas para brincar'
     print 'A mamãe gritou'
     patos -= 1
     quack()
     if patos > 1:
-        print 'Mas só ' + str(patos) + (' patinhos voltaram de lá.\n' if
-            patos != 1 else ' patinho voltou de lá.\n')
+        print 'Mas só ' + str(patos) + (' patinhos voltaram de lá.\n' if patos != 1 else ' patinho voltou de lá.\n')
     else:
         print 'Mas nenhum patinho voltou de lá.\n'
 
