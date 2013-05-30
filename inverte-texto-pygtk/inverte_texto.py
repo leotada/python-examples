5fe91fe5da6be1264c99f13f5798687588fec212
leo523 <leonardo_tada@hotmail.com>
2013-05-30 18:53:05 -0300

    Create inverte_texto.py

---
+++ inverte-texto-pygtk/inverte_texto.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import sys
+
+#Arquivo glade, pode ser editado com o Glade.
+UI_FILE = "inverte_texto.ui"
+
+class GUI:
+  def __init__(self):
+
+		self.builder = gtk.Builder()
+		self.builder.add_from_file(UI_FILE)
+		self.builder.connect_signals(self)
+
+		window = self.builder.get_object('window')
+		self.campo = self.builder.get_object('entry1')
+
+		window.show_all()
+
+	def botao_inverter(self, botao):
+		texto = self.campo.get_text()
+		self.inverte(texto)
+		
+	def inverte(self, texto):
+		final = texto[::-1]
+		self.campo.set_text(final)
+				
+	def sair(window, self):
+		gtk.main_quit()
+	
+	def destroy(window, self):
+		gtk.main_quit()
+
+def main():
+	app = GUI()
+	gtk.main()
+		
+if __name__ == "__main__":
+    sys.exit(main())
